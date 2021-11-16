f32d642ba63a6f9b6f9b265e56328de57a0e6fda
SadafZamonova <84777664+SadafZamonova@users.noreply.github.com>
2021-11-17 00:52:00 +0500

    добавила api

--- first/index.js
+++ first/index.js
@@ -1,7 +1,7 @@
 
 document.querySelector('button').addEventListener('click', (evt) => {
     evt.preventDefault();
-    fetch("https://api.giphy.com/v1/gifs/search?api_key=WVWFB4UHkhAg5qfvQRkIw9FY5CnRMHkr&limit=5&offset=0&rating=g&lang=ru")
+    fetch("https://api.giphy.com/v1/gifs/search?api_key=ZClm4bxlZZbX6aLHMLRss3N0LPd2UHoa&q=&limit=5&offset=0&rating=g&lang=en")
         .then(response => response.json())
         .then( user =>{
             document.querySelector(".output").src = user.avatar_url;
