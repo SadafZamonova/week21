22e350d8c16a80cf49182c4da9709d33db4df2e6
SadafZamonova <84777664+SadafZamonova@users.noreply.github.com>
2021-11-18 21:38:40 +0500

    котики вышли))

--- first/index.js
+++ first/index.js
@@ -1,10 +1,21 @@
 
-document.querySelector('button').addEventListener('click', (evt) => {
+document.querySelector('#button').addEventListener('click', (evt) => {
     evt.preventDefault();
-    fetch("https://api.giphy.com/v1/gifs/search?api_key=ZClm4bxlZZbX6aLHMLRss3N0LPd2UHoa&q=&limit=5&offset=0&rating=g&lang=en")
+   let search = document.querySelector("#Search").value;
+    fetch("https://api.giphy.com/v1/gifs/search?api_key=ZClm4bxlZZbX6aLHMLRss3N0LPd2UHoa&q=" + search + "&limit=5&offset=0&rating=g&lang=en")
         .then(response => response.json())
         .then( user =>{
-            document.querySelector(".output").src = user.avatar_url;
+            console.log(user);
+        let cat=document.querySelector(".output");
+        cat.innerHTML= " ";
+
+        for (item of user.data) {
+            let title = document.createElement("h1");
+            title.innerHTML+=item.title;
+            let img = document.createElement("img");
+            img.src=item.images.original.url;
+            cat.append(title, img);
+        }
         })
         .catch(error => console.log(error));
 })
