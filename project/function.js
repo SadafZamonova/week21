d8f0578674c3db4bd99de446497703b98c3fdc1f
SadafZamonova <84777664+SadafZamonova@users.noreply.github.com>
2021-11-27 16:05:47 +0500

    избранные

--- project/function.js
+++ project/function.js
@@ -2,15 +2,10 @@ document.addEventListener("DOMContentLoaded", function (event) {
     const detailsObject = JSON.parse(localStorage.getItem("details"));
     console.log(detailsObject);
     document.querySelector("#title").innerHTML = detailsObject.strDrink;
-    document.querySelector("#img").src =detailsObject.strDrinkThumb;
+    document.querySelector("#img").src = detailsObject.strDrinkThumb;
     document.querySelector("#ingr").innerHTML = detailsObject.strMeasure1 + detailsObject.strIngredient1;
     img.style.width = '300px';
-            img.style.height = '300px';
-            
-document.querySelector("#submit").addEventListener("click", (evt) => {
-    evt.preventDefault();
-    localStorage.setItem("details", JSON.stringify(detailsObject));
-    document.location.href = "function.html";
-            
+    img.style.height = '300px';
+
+    
 })
-});
