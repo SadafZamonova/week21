57aef77e7debb1882df47b5d672ac0e030fba6d1
SadafZamonova <84777664+SadafZamonova@users.noreply.github.com>
2021-11-27 23:42:04 +0500

    вывод в консоль

--- project/details.js
+++ project/details.js
@@ -2,15 +2,25 @@ document.addEventListener("DOMContentLoaded", function (event) {
     const detailsObject = JSON.parse(localStorage.getItem("details"));
     console.log(detailsObject);
     document.querySelector("#title").innerHTML = detailsObject.strDrink;
-    document.querySelector("#img").src =detailsObject.strDrinkThumb;
+    document.querySelector("#img").src = detailsObject.strDrinkThumb;
     document.querySelector("#ingr").innerHTML = detailsObject.strMeasure1 + detailsObject.strIngredient1;
     img.style.width = '300px';
-            img.style.height = '300px';
-            
-document.querySelector("#submit").addEventListener("click", (evt) => {
-    evt.preventDefault();
-    localStorage.setItem("favorites", JSON.stringify(detailsObject));
-    document.location.href = "favorites.html";
-            
-})
+    img.style.height = '300px';
+
+    document.querySelector("#submit").addEventListener("click", (evt) => {
+        evt.preventDefault();
+
+        let favoritesFromStorage = JSON.parse(localStorage.getItem("favorites"));
+         console.log(favoritesFromStorage);
+        if (!Array.isArray(favoritesFromStorage)) {
+            favoritesFromStorage = [];
+        }
+
+        favoritesFromStorage.push(detailsObject);
+
+        localStorage.setItem("favorites", JSON.stringify(favoritesFromStorage));
+
+       // document.location.href = "favorites.html";
+
+    })
 });
