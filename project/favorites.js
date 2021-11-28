456423c54cc813b791146c0b4795d6f749396583
SadafZamonova <84777664+SadafZamonova@users.noreply.github.com>
2021-11-28 15:59:01 +0500

    исправила массив

--- project/favorites.js
+++ project/favorites.js
@@ -4,13 +4,13 @@ document.addEventListener("DOMContentLoaded", function (event) {
     console.log(favoritesObject);
 
 
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i <  favoritesObject.length; i++) {
         let title = document.createElement("h1");
         let ingr = document.createElement("p");
         let img = document.createElement("img");
-        title.innerHTML = favoritesObject.strDrink;
-        ingr.innerHTML = favoritesObject.strMeasure1 + favoritesObject.strIngredient1;
-        img.src = favoritesObject.strDrinkThumb;
+        title.innerHTML = favoritesObject[i].strDrink;
+        ingr.innerHTML = favoritesObject[i].strMeasure1 + favoritesObject[i].strIngredient1;
+        img.src = favoritesObject[i].strDrinkThumb;
 
     img.style.width = '300px';
     img.style.height = '300px';
